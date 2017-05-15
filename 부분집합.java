e0d5508a6d93f8c349735d44f7d16ee8c6181095
pyjune <pyjune@naver.com>
2017-05-15 12:03:19 +0900

    Update 부분집합.java

--- 부분집합.java
+++ 부분집합.java
@@ -1,1 +1,33 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
 
+public class Solution {
+	static int[] a = {1,2,3};
+	static int[] l = new int[3];
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		subset(0, 3);
+	
+	}
+	public static void subset(int n, int k)
+	{
+		if(n==k)
+		{
+			for(int i=0; i<k; i++)
+			{
+				//System.out.print(l[i]);
+				if(l[i]==1)
+					System.out.print(a[i]);
+			}
+			System.out.println();
+		}
+		else
+		{
+			l[n] = 0;
+			subset(n+1, k);
+			l[n] = 1;
+			subset(n+1, k);
+			
+		}
+	}
+}
